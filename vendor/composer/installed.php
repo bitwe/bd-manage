c6fa664459c91f54456469a11d59aa21182f6005
Daniel Novo <daniel.novo@bitwe.pt>
2022-01-19 14:25:09 +0000

    v1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3ff09205274d4da74e0965901cb7cb99d92a1826',
+    'name' => 'bitwe/bd-manage',
+  ),
+  'versions' => 
+  array (
+    'bitwe/bd-manage' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ff09205274d4da74e0965901cb7cb99d92a1826',
+    ),
+  ),
+);
